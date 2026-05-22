f6b4162af71142883232905f131292a76752d1c1
Oleg <35769958+trudoholic@users.noreply.github.com>
2026-05-22 22:12:40 +0300

    form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import {Header} from "./components/Header";
+import {HabitForm} from "./components/HabitForm";
 
 function App() {
   return (
     <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
       <Header/>
+      <HabitForm/>
     </div>
   )
 }
---
+++ src/components/HabitForm.tsx
@@ -0,0 +1,11 @@
+import {Button} from "./Button";
+
+export function HabitForm() {
+  return (
+    <form className="flex gap-2">
+      <input className="flex-1 rounded-lg bg-zinc-800 px-4 py-2 outline-none
+      focus-visible:ring-2 focus-visible:ring-violet-500" placeholder="New Habit..." />
+      <Button>Add Habit</Button>
+    </form>
+  )
+}
