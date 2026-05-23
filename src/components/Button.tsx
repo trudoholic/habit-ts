e40fb52de66d6f01b54a7dbb58689b04c8656220
Oleg <35769958+trudoholic@users.noreply.github.com>
2026-05-23 23:03:03 +0300

    comp props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,22 @@
-import type {ReactNode} from "react";
+// import type {ComponentProps, ReactNode} from "react";
+import type {ComponentProps} from "react";
 
 type ButtonProps = {
   // text: string
-  children: ReactNode
-  disabled?: boolean
-}
+  // children: ReactNode
+  // disabled?: boolean
+} & ComponentProps<"button">
 
 // export function Button({text}: ButtonProps) {
-export function Button({children, disabled = false}: ButtonProps) {
+// export function Button({children, disabled = false}: ButtonProps) {
+export function Button({...props}: ButtonProps) {
   return (
     <button
-      disabled={disabled}
+      // disabled={disabled}
+      {...props}
       className="bg-violet-600 hover:bg-violet-500 transition-colors rounded px-2 py-1
       disabled:opacity-30 disabled:cursor-not-allowed">
-      {children}
+      {/*{children}*/}
     </button>
   )
 }
