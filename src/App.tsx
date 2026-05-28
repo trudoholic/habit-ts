7dfd43d789c3060307b65f4f90f1eaaccf0b268e
Oleg <35769958+trudoholic@users.noreply.github.com>
2026-05-28 20:56:04 +0300

    dates

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import {HabitProvider} from "./context/HabitProvider";
+import {useState} from "react";
 import {Header} from "./components/Header";
 import {HabitForm} from "./components/HabitForm";
 import {HabitList} from "./components/HabitList";
+import {addWeeks, eachDayOfInterval, endOfWeek, startOfWeek} from "date-fns";
 
 function App() {
+  const [weekOffset, setWeekOffset] = useState(0)
+
+  const week = addWeeks(new Date(), weekOffset)
+
+  const visibleDates = eachDayOfInterval({
+    start: startOfWeek(week, {weekStartsOn: 1}),
+    end: endOfWeek(week, {weekStartsOn: 1})
+  })
+
   return (
     <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
       <HabitProvider>
-        <Header/>
+        <Header
+          visibleDates={visibleDates}
+          onNext={() => setWeekOffset(o => o + 1)}
+          onPrev={() => setWeekOffset(o => o - 1)}
+        />
         <HabitForm />
         <HabitList />
       </HabitProvider>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,31 @@
 import {Button} from "./Button";
+import {useHabits} from "../context/useHabits";
+import {format, isToday} from "date-fns";
 
-export function Header() {
-  // return <h1 className="text-3xl font-bold underline">Hello world!</h1>
-  // return <h1 className="text-red-500">Hello world!</h1>
+type HeaderProps = {
+  visibleDates: Date[]
+  onPrev: () => void
+  onNext: () => void
+}
+
+export function Header({visibleDates, onPrev, onNext}: HeaderProps) {
+  const {habits} = useHabits()
+
+  const doneToday = habits.filter(h => h.completions.some(c => isToday(c))).length
+  const dateRange =
+    `${format(visibleDates[0], "MMM d")} - ${format(visibleDates.at(-1)!, "MMM d")}`
+
   return (
     <header className="flex items-center justify-between">
       <div className="flex flex-col gap-1">
         <h1 className="text-3xl font-bold">Habit Tracker</h1>
-        <span className="text-zinc-400 text-sm">1 / 1 done today</span>
+        <span className="text-zinc-400 text-sm">{doneToday} / {habits.length} done today</span>
       </div>
       <div className="flex flex-col gap-1 items-end">
-        <span className="text-zinc-400 text-sm">May 25 - May 31</span>
+        <span className="text-zinc-400 text-sm">{dateRange}</span>
         <div className="flex items-center gap-3">
-          {/*<Button text="Prev"/>*/}
-          {/*<Button text="Next"/>*/}
-          <Button>Prev</Button>
-          <Button>Next</Button>
+          <Button onClick={onPrev} >Prev</Button>
+          <Button onClick={onNext} disabled={visibleDates.some(d => isToday(d))} >Next</Button>
         </div>
       </div>
     </header>
